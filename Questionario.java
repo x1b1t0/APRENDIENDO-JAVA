62d2e206c12d9ac5906bc6e457497262d65d256b
x1b1t0 <ivaanv23@gmail.com>
2025-10-28 14:31:18 +0000

    Add Questionario Java program with input validation

---
+++ Questionario.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Questionario {
+    public static void main(String[] args) {
+        System.out.println("Benvingut al qüestionari!");
+
+        Scanner newscanner = new Scanner(System.in);
+
+        System.out.println("Quin es el teu llenguatge de programacio preferit?");
+        String resposta = newscanner.nextLine();
+        System.out.println("Resposta: " + resposta);
+
+        System.out.println("Quants anys tens?");
+        int edat = newscanner.nextInt();
+        System.out.println("Edat: " + edat);
+
+        System.out.println("Ets home o dona (Indica amb H o D)?");
+        char genere = newscanner.next().charAt(0);
+        System.out.println("Genere: " + genere);
+
+        if (genere == 'H' || genere == 'h') {
+            System.out.println("Has indicat que ets home.");
+        } else if (genere == 'D' || genere == 'd') {
+            System.out.println("Has indicat que ets dona.");
+        } else {
+            System.out.println("Genere no reconegut.");
+        }
+
+        System.out.println("Gracies per participar al qüestionari!");
+
+        newscanner.close();
+    }
+}
