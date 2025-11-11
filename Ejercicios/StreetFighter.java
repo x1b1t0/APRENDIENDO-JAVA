ab3b2069386e7ca30d722104b13c08420e55ad57
x1b1t0 <ivaanv23@gmail.com>
2025-11-11 15:03:03 +0000

    Primer commit

--- Ejercicios/StreetFighter.java
+++ Ejercicios/StreetFighter.java
@@ -7,13 +7,26 @@ public class StreetFighter {
     public static void main (String [] args) {
         
         Scanner sc = new Scanner(System.in);
-        string estadoactual = nextLine(),next();
-        String movimiento = sc.nextLine().next();
+        String estadoactual = sc.nextLine();
+        String movimiento = sc.nextLine();
 
+        if (estadoactual.equals("JUMPING")) {
+            
+            } else if movimiento.equals("TOUCH_FLOOR") {
+            
+            } else if (movimiento.equals("PUNCH_KEY")) {
+            
+            } else if (movimiento.equals("JOYSTICK_CENTER) {
+            
+            } else if (movimiento.equals("KICK_KEY")){
 
+            
+            }
+            
+        }
         
         if (movimiento.equals("IDLE JOYSTICK_UP")) {
-            System.out.println("IDLE");
+            System.out.println("JUMPING");
         } else if (movimiento.equals("IDLE JOYSTICK_LEFT/RIGHT")) {
             System.out.println("WALKING");
         } else if (movimiento.equals("IDLE JOYSTICK_CENTER")) {
