295000a7a75d7097723d16bdb98643ab2b5a15a3
x1b1t0 <ivaanv23@gmail.com>
2025-11-14 17:20:18 +0000

    Primer commit

---
+++ Apuntes/Condicionales.java
@@ -0,0 +1,5 @@
+package Apuntes;
+
+public class Condicionales {
+    
+}
---
+++ Ejercicios/Parquing.java
@@ -0,0 +1,52 @@
+package Ejercicios;
+
+import java.util.Scanner;
+
+public class Parquing {
+
+    public static void main (String [] args) }
+
+    String matricula;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce la matricula del coche: ");
+        matricula = sc.nextLine();
+
+    String TipusVehicle;
+    System.out.println("Introduce el tipo de vehiculo (Coche/Moto): ");
+    TipusVehicle = sc.nextLine();
+
+    //Ahora hara falta poner los tipos de estacionamiento y las tarifas, dependiendo si es coche o moto
+
+    // Coche
+
+    // Tipus A) Per hora, el valor de la qual és de 3€ per hora
+    // Tipus B) Mitja jornada (8 hores), el valor total de la qual és de 15€ i posseeix un 5% de descompte.
+    // Tipus C) Jornada completa (16 hores), el valor total de la qual és de 30€ i té un 5% de descompte
+
+    // Moto
+    // Tipus A) Per hora, el valor de la qual és de 1€ per hora
+    // Tipus B) Mitja jornada (8 hores), el valor total de la qual és de 5€
+    // Tipus C) Jornada completa (16 hores), el valor total de la qual és de 10€
+  
+    String TiposEstacionamiento;
+    System.out.println("Introduce el tipo de estacionamiento (A/B/C): ");
+    TiposEstacionamiento = sc.nextLine();
+
+    if (TipusVehicle.equals("Coche"));
+        if (TiposEstacionamiento.equals("A"))
+            System.out.println("El precio es de 3€ por hora");
+        else if (TiposEstacionamiento.equals("B"))
+            System.out.println("El precio es de 15€ aplicando un 5% de descuento");
+        else if (TiposEstacionamiento.equals("C"))
+            System.out.println("El precio es de 30€ aplicando un 5% de descuento");
+
+    if (TipusVehicle.equals("Moto"));
+        if (TiposEstacionamiento.equals("A"))
+            System.out.println("El precio es de 1€ por hora");
+        else if (TiposEstacionamiento.equals("B"))
+            System.out.println("El precio es de 5€");
+        else if (TiposEstacionamiento.equals("C"))
+            System.out.println("El precio es de 10€");
+
+    }         
+}
