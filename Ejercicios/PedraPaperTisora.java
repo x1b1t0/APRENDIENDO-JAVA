8375018527cffb77ffc60ae0fb250f18270d559a
x1b1t0 <ivaanv23@gmail.com>
2025-11-25 14:56:40 +0000

    Primer commit

---
+++ Ejercicios/PedraPaperTisora.java
@@ -0,0 +1,18 @@
+package Ejercicios;
+public class PedraPaperTisora {
+    import java.util.Scanner;
+// Per acabar el joc de Pedra, Paper o Tisora
+// El caracter @ significa "Pedra"
+// El caracter # significa "Paper"
+// El caracter % significa "Tisora"
+
+System.out.println("Introdueix la teva elecció (Pedra '@', Paper '#', Tisora '% o (END) si vols sortir del programa'):");
+
+Scanner sc = new Scanner(System.in);
+String eleccioJugador = sc.nextLine();
+while (eleccioJugador != "END") {
+    String[] opcions = {"@", "#", "%"};
+
+
+
+}
