1f2ce4c07b8a36fe404334d6414e0cee5062876f
x1b1t0 <ivaanv23@gmail.com>
2026-01-16 17:18:31 +0000

    Primer commit

--- Ejercicios/calculodescuento.java
+++ Ejercicios/calculodescuento.java
@@ -4,6 +4,10 @@ public class calculodescuento {
         float[] precios = {50f, 49.99f, 25.45f};
         float[] descuentos = {10f, 21f, 17.89f};
 
-        for int 
+
+        for (int i = 0; i < precios.length; i++) {
+            float resultado = precios[i] * (1 - descuentos[i] / 100);
+            System.out.println("Resultado " + (i + 1) + ": " + resultado);
+        }
     }
-}
+}    
